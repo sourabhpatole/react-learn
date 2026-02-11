a857dd2d192a29d33883d513bfbf3d12a6286726
Sourabh <sourabhpatole@outlook.com>
2026-02-11 11:06:26 +0530

    real time render

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [inputVal, setInputVal] = useState("");
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <input
+        type="text"
+        placeholder="Enter something"
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+      />
+      <p>
+        You Typed : <strong>{inputVal}</strong>
+      </p>
     </div>
   );
 }
